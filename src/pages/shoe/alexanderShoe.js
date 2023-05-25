3466c70da4947de4271a594a57d4129595dbb556
Akash <akash@gmail.com>
2023-05-25 16:57:00 +0545

    Hanger/E-comerce

---
+++ src/pages/shoe/alexanderShoe.js
@@ -0,0 +1,75 @@
+
+// alexander shoe collection
+const image = require("../../upload/alexander/Large-705071W4W118395_F.jpg");
+const image2 = require("../../upload/alexander/Large-727392WICG69026_F.jpg");
+const image3 = require("../../upload/alexander/Large-688548WIC949000_E.jpg");
+const image4 = require("../../upload/alexander/Large-727394WHXMT1643_F.jpg");
+const image5 = require("../../upload/alexander/Large-727405W4RY14203_F.jpg");
+const image6 = require("../../upload/alexander/Large-735773WHGPC1688_D.jpg");
+const image7 = require("../../upload/alexander/Large-735775WICYX9061_R.jpg");
+const image8 = require("../../upload/alexander/Large-735773WHGPC1688_F.jpg");
+
+
+
+
+export const shoesDetails = [{
+    id: 1,
+    title: "Alexander McQueen",
+    body: "Price",
+    price: 299,
+    image: image,
+},
+{
+    id: 2,
+    title: "Alexander McQueen",
+    body: "Price",
+    price: 299,
+    image: image2,
+},
+{
+    id: 3,
+    title: "Alexander McQueen",
+    body: "Price",
+    price: 299,
+    image: image3,
+},
+{
+    id: 4,
+    title: "Alexander McQueen",
+    body: "Price",
+    price: 299,
+    image: image4,
+},
+{
+    id: 5,
+    title: "Alexander McQueen",
+    body: "Price",
+    price: 299,
+    image: image5,
+},
+{
+    id: 6,
+    title: "Alexander McQueen",
+    body: "Price",
+    price: 299,
+    image: image6,
+},
+{
+    id: 7,
+    title: "Alexander McQueen",
+    body: "Price",
+    price: 299,
+    image: image7,
+},
+{
+    id: 8,
+    title: "Alexander McQueen",
+    body: "Price",
+    price: 299,
+    image: image8,
+},
+
+]
+
+
+
---
+++ src/pages/shoe/nike.js
@@ -0,0 +1,75 @@
+
+// alexander shoe collection
+const image8 = require("../../upload/nike/air-force-1-07-mens-shoes-jBrhbr.png");
+const image2 = require("../../upload/nike/air-jordan-6-retro-shoes-QLGPHR.png");
+const image3 = require("../../upload/nike/air-max-90-mens-shoes-6n3vKB.png");
+const image4 = require("../../upload/nike/air-vapormax-2023-flyknit-mens-shoes-3q1qZg.png");
+const image5 = require("../../upload/nike/men-s-shoes-clothing-accessories (2).jpg");
+const image6 = require("../../upload/nike/men-s-shoes-clothing-accessories (3).jpg");
+const image7 = require("../../upload/nike/men-s-shoes-clothing-accessories (4).jpg");
+
+
+
+
+export const Nikedetails = [
+    //     {
+    //     id: 1,
+    //     title: "Nike",
+    //     body: "Price",
+    //     price: 299,
+    //     image: image,
+    // },
+    {
+        id: 2,
+        title: "Nike",
+        body: "Price",
+        price: 299,
+        image: image2,
+    },
+    {
+        id: 3,
+        title: "Nike",
+        body: "Price",
+        price: 299,
+        image: image3,
+    },
+    {
+        id: 4,
+        title: "Nike",
+        body: "Price",
+        price: 299,
+        image: image4,
+    },
+    {
+        id: 5,
+        title: "Nike",
+        body: "Price",
+        price: 299,
+        image: image5,
+    },
+    {
+        id: 6,
+        title: "Nike",
+        body: "Price",
+        price: 299,
+        image: image6,
+    },
+    {
+        id: 7,
+        title: "Nike",
+        body: "Price",
+        price: 299,
+        image: image7,
+    },
+    {
+        id: 8,
+        title: "Nike",
+        body: "Price",
+        price: 299,
+        image: image8,
+    },
+
+]
+
+
+
